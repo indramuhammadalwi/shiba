64c3b460f20b8443c09722badf062c9bbbc94c84
indramuhammadalwi <indramuhammadalwi@gmail.com>
2022-01-07 22:30:10 +0700

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -1,5 +1,6 @@
 !/bin/sh
-sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+sudo apt update
+sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 git clone https://github.com/xmrig/xmrig.git
 mkdir xmrig/build && cd xmrig/build
 cmake ..
